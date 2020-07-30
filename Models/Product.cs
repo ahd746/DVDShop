1d6df0a6eb759d0f7a7c7fe526a2fb2e747c2659
Ahmed Salama <eng.ahd746@gmail.com>
2020-07-29 22:30:34 -0400

    Phase one completed

---
+++ Models/Product.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace DVDShop.Models
+{
+    public partial class Product
+    {
+        public Product()
+        {
+            Cart = new HashSet<Cart>();
+            OrderDetail = new HashSet<OrderDetail>();
+        }
+
+        [Key]
+        public int ProductId { get; set; }
+        [Required]
+        [StringLength(255)]
+        public string Name { get; set; }
+        [StringLength(8000)]
+        public string Description { get; set; }
+        [Column(TypeName = "decimal(10, 2)")]
+        [Range(0,9999999)]
+        [DisplayFormat(DataFormatString ="{0:c}")]
+        public decimal Price { get; set; }
+        public int? Year { get; set; }
+        public int CategoryId { get; set; }
+
+        [ForeignKey(nameof(CategoryId))]
+        [InverseProperty("Product")]
+        public virtual Category Category { get; set; }
+        [InverseProperty("Product")]
+        public virtual ICollection<Cart> Cart { get; set; }
+        [InverseProperty("Product")]
+        public virtual ICollection<OrderDetail> OrderDetail { get; set; }
+    }
+}
